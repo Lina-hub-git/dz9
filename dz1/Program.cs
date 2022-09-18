687d5902018f782a74efe1faa66c61fd28af0781
Angelina <Angel7@gmail.com>
2022-09-18 19:45:17 +0300

    первая задача

---
+++ dz1/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите два числа");
+int a=Console.ReadLine();
+int b=Console.ReadLine();
+if (a>b)
+{
+    Console.WriteLine("число а больше числа b");
+}
+else
+{
+    Console.WriteLine("число b больше числа a");
+}

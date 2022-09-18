6a38aba675c75f6792c2aa87d0afe3acbe3e38f0
Angelina <Angel7@gmail.com>
2022-09-18 22:50:36 +0300

    третья задача

--- dz1/Program.cs
+++ dz1/Program.cs
@@ -1,11 +1,65 @@
-﻿Console.WriteLine("Введите два числа");
-int a=Console.ReadLine();
-int b=Console.ReadLine();
-if (a>b)
+﻿// Первая задача
+
+// Console.WriteLine("Введите два числа");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int b = Convert.ToInt32(Console.ReadLine());
+// if (a>b)
+// {
+//     Console.WriteLine("число а = " + a + " больше числа b = " + b);
+// }
+// else if (a==b)
+// {
+//     Console.WriteLine("число b = " + b + " равно числу a = " + a);
+// }
+// else
+// {
+//     Console.WriteLine("число b = " + b + " больше числа a = " + a);
+// }
+
+
+
+
+
+
+
+
+
+
+// Вторая задача
+
+// int max=0;
+// int Power (int r,int t)
+// {
+// if (r>=t) 
+// {
+//     max=r;
+//     return max;
+// }
+// else 
+// {
+//     max=t;
+//     return max;
+// }
+// }
+// Console.WriteLine("Введите три числа");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int b = Convert.ToInt32(Console.ReadLine());
+// int c = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine(Power(a,Power(b,c)));
+
+
+
+
+// третья задача
+
+Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a%2==0)
 {
-    Console.WriteLine("число а больше числа b");
+    Console.WriteLine("число   " + a + " четное");
 }
 else
 {
-    Console.WriteLine("число b больше числа a");
+    Console.WriteLine("число   " + a + " нечетное");
 }

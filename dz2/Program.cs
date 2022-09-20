f1358e550f010388fc53c39cd131558d0ee60717
Angelina <Angel7@gmail.com>
2022-09-20 23:28:00 +0300

    третья  задача0

--- dz2/Program.cs
+++ dz2/Program.cs
@@ -1,7 +1,48 @@
-﻿Console.WriteLine("Введите  трехзначное число Num");
-int Num = Convert.ToInt32(Console.ReadLine());
+﻿void Zadacha1()
+{
+
+    Console.WriteLine("Введите  трехзначное число Num");
+    int Num = Convert.ToInt32(Console.ReadLine());
 
 
-Num1=Num/10%10;
-Console.WriteLine(Num1);
+    int Num1=Num/10%10;
+    Console.WriteLine(Num1);
+}
+
+
+void Zadacha2()
+{
+
+    Console.WriteLine("Введите  трехзначное число Num");
+    int Num = Convert.ToInt32(Console.ReadLine());
+
+
+    int Num1=Num/10%10;
+    Console.WriteLine(Num1);
+}
+
+
+
+void Zadacha3()
+{
+
+    Console.WriteLine("Введите число от 1 до 7");
+    int DenNedeli = Convert.ToInt32(Console.ReadLine());
+
+    if ((DenNedeli==6) || (DenNedeli==7))
+    {
+        Console.WriteLine("выходной день");
+    }
+    else
+    {
+        Console.WriteLine("рабочий день");
+    }
+    
+}
+
+
+
+
+
+Zadacha3();
 

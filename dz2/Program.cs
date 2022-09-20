b0bbc6a3449a42faf6e4c182650c61ac5208dbaa
Angelina <Angel7@gmail.com>
2022-09-21 00:06:19 +0300

    вторая  задача0

--- dz2/Program.cs
+++ dz2/Program.cs
@@ -13,13 +13,59 @@
 void Zadacha2()
 {
 
-    Console.WriteLine("Введите  трехзначное число Num");
-    int Num = Convert.ToInt32(Console.ReadLine());
+    // Console.WriteLine("Введите  число");
+    // int i;
+    //         int[] array = new int [10];
+    //         i = 0;
+    //         do
+    //         {
+    //             array[i] = Convert.ToInt32(Console.ReadLine());
+    //             i=i+1;
+    //         }
+    //         while (Console.ReadLine() != "end");
 
 
-    int Num1=Num/10%10;
-    Console.WriteLine(Num1);
+
+    
+    // int n=array.Length;
+    // int index=2;
+    // Console.WriteLine(array[index]);
+
+
+// Задать с клавиатуры число элементов массива
+        int n;
+        Console.WriteLine("Введите число элементов числа");
+        n = Convert.ToInt16(Console.ReadLine());
+     // Объявим одномерный массив Data с n-элементами 
+        int [] Data = new int[n];
+        int i = 0;
+       while (i < n)
+        {
+           Console.WriteLine("Введите цифры числа поочередно");
+        /* Вводим элементы массива с клавиатуры 
+         * и заполняем ими массив */
+           Data[i] = int.Parse(Console.ReadLine());
+           Console.WriteLine();
+           i++;
+        }
+   // Вывод содержимого массива
+     for (i = 0; i < n; i = i + 1)
+     Console.WriteLine("Элемент[" + i + "]: " + Data[i]);
+    
+
+
+    
+    if (n<3) 
+    {
+        Console.WriteLine("третьей цифры нет");
+    }
+    else
+    {
+        Console.WriteLine(Data[2]); 
+    }
+
 }
+
 
 
 
@@ -44,5 +90,6 @@ void Zadacha3()
 
 
 
-Zadacha3();
+Zadacha2();
+
 

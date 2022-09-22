57c980aa0f8e50459d929c499796ff8018ea9c9b
Angelina <Angel7@gmail.com>
2022-09-22 23:06:56 +0300

    первая  задача

---
+++ dz3/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите пятизначное число");
+int t = Convert.ToInt32(Console.ReadLine());
+
+int t1=t/10000;
+int t2=t/1000%10;
+int t4=t/10%10;
+int t5=t%10;
+
+Console.WriteLine(t1,t2,t4,t5);

caf809615be6bda26692f9ba696af3348f6d6193
Angelina <Angel7@gmail.com>
2022-09-30 01:21:36 +0300

    3  задача

--- dz4/Program.cs
+++ dz4/Program.cs
@@ -32,10 +32,68 @@ current_num=current_num/10;
 i++;
 }
 Console.WriteLine($"Сумма числа {num} равна: " + S);
+}
 
 
 
-Zadacha2();
+
+void Zadacha3()
+{
+int size=8;    
+int[] array=new int[size];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+SelectionSort(array);
+PrintArray(array);
+}
+
+void FillArray(int[] collection)
+{
+    int l=collection.Length;
+    int i=0;
+    while (i<l)
+    {
+        collection[i]=new Random().Next(-50,50);
+        i++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int d=col.Length;
+    int i=0;
+    while (i<d)
+    {
+        Console.Write(col[i] + "  ");
+        i++;
+    }
+}
+
+
+void SelectionSort(int[] numb)
+{
+    for(int i=0; i<numb.Length-1; i++)
+    {
+        int min=i;
+        for(int j=i+1; j<numb.Length; j++)
+        {
+            if(Math.Abs(numb[j])<Math.Abs(numb[min]))
+            {
+                min=j; 
+            }
+        }
+        int t=numb[i];
+        numb[i]=numb[min];
+        numb[min]=t;
+    }
+}
+
+
+
+
+
+Zadacha3();
 
 
 

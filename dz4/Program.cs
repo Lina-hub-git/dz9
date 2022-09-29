5c0bf60b4dd982a3791e56387df21dcc84eaeb77
Angelina <Angel7@gmail.com>
2022-09-29 23:42:50 +0300

    1  задача

---
+++ dz4/Program.cs
@@ -0,0 +1,31 @@
+﻿void Zadacha1()
+{
+Console.WriteLine("Введите число A");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B");
+int B = Convert.ToInt32(Console.ReadLine());
+
+int i=1; 
+double t=A;
+
+while (i<B)
+{
+    double t = t*A;
+    Console.WriteLine(t);
+    i=i+1;
+}
+
+
+}
+
+
+
+
+
+
+
+
+Zadacha1();
+
+
+

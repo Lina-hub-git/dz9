35d58026f13a6e182f53dc5cd1d68a4be5556138
Angelina <Angel7@gmail.com>
2022-09-30 20:45:42 +0300

    34  задача

---
+++ dz5/Program.cs
@@ -0,0 +1,55 @@
+﻿void Zadacha1()
+{
+int size=8;    
+int[] array=new int[size];
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+ChetnieSort(array);
+
+}
+
+void FillArray(int[] collection)
+{
+    int l=collection.Length;
+    int i=0;
+    while (i<l)
+    {
+        collection[i]=new Random().Next(100,1000);
+        i++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int d=col.Length;
+    int i=0;
+    while (i<d)
+    {
+        Console.Write(col[i] + "  ");
+        i++;
+    }
+}
+
+
+int ChetnieSort(int[] numb)
+{
+    for(int i=0; i<numb.Length; i++)
+    {
+        k=0;
+        if (numb[i]%2==0)
+            k++;
+    }
+    Console.WriteLine(k);
+
+}
+
+
+
+
+
+Zadacha1();
+
+
+
+

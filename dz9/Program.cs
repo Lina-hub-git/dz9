676914bad6f332418edc0de66320ce65923ef580
Angelina <Angel7@gmail.com>
2022-10-15 00:39:26 +0300

    3  задача

--- dz9/Program.cs
+++ dz9/Program.cs
@@ -43,5 +43,28 @@ int Summa(int m, int n,  int sum=0)
 }
 
 
-Zadacha2();
+
+
+
+
+void Zadacha3()
+{ 
+   
+Console.WriteLine("задайте положительное число M");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Задайте положительное число N");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(A(m,n));
+}
+
+int A(int m, int n)
+{   
+    if (m==0) return n+1;
+    else if (m>0 && n==0) return A(m-1,1);
+          else if (m>0 && n>0) return A(m-1,A(m,n-1));  
+    return A(m,n);
+}
+
+
+Zadacha3();
 
